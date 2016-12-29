c0512e9474cdaf90eb37fbe99d1baa6128db6fed
matt napper <nappdaddy2000@gmail.com>
2016-12-29 03:42:32 +0000

    Added contact form mailer view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "pages#home"
   get '/about' => 'pages#about'
-  resources :contacts
+  resources :contacts, only: [:create]
   get 'contact-us' => 'contacts#new'
 end
