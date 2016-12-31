98c8df1c0e7623b1741d9000dda5d6cd269f1238
matt napper <nappdaddy2000@gmail.com>
2016-12-31 04:58:31 +0000

    added code commients for stripe

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,11 +1,15 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+#extend deafult behavior so users signing up with pro account save with a special stripe subscription function
+#else user signs up normally
   def create
     super do |resource|
       if params[:plan]
         resource.plan_id = params[:plan]
         if resource.plan_id == 2
+#if pro user pass validations, then calls stripe, and stripe sets up a subscription, and charges customers card
           resource.save_with_subscription
         else
+#normal style save for basic
           resource.save
         end
       end
